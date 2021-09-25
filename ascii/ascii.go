f03f9be29c54b97e4ec5f4e68ab375df730fa0fc
Jose Coelho <jose.alberto.coelho@gmail.com>
2021-09-25 21:07:50 +0100

    Implement BIC parse and validation

--- ascii/ascii.go
+++ ascii/ascii.go
@@ -16,6 +16,10 @@ func IsAlphaNumeric(r rune) bool {
 	return IsDigit(r) || IsLowerCase(r) || IsUpperCaseLetter(r)
 }
 
+func IsUpperAlphaNumeric(r rune) bool {
+	return IsDigit(r) || IsUpperCaseLetter(r)
+}
+
 func Every(s string, fn func(r rune) bool) bool {
 	for _, r := range s {
 		if !fn(r) {
---
+++ bic/doc.go
@@ -0,0 +1,4 @@
+package bic
+
+// BIC represents Business Identifier Code
+// The standard is defined in ISO 9362:2014
