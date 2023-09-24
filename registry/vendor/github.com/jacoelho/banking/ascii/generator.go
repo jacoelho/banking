38583a47f42417beccc6b2d9434a762bbd1d0d2e
Jose Coelho <jose.alberto.coelho@gmail.com>
2023-09-24 10:42:33 +0100

    chore: vendor

---
+++ registry/vendor/github.com/jacoelho/banking/ascii/generator.go
@@ -0,0 +1,38 @@
+package ascii
+
+import (
+	"math/rand"
+)
+
+const (
+	digits            = "0123456789"
+	lowerCaseLetters  = "abcdefghijklmnopqrstuvwxyz"
+	upperCaseLetters  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	alphaUpperNumeric = digits + upperCaseLetters
+)
+
+type RuneWriter interface {
+	WriteRune(r rune) (int, error)
+}
+
+func Digits(sb RuneWriter, length int) {
+	stringWithCharset(sb, digits, length)
+}
+
+func LowerCaseLetters(sb RuneWriter, length int) {
+	stringWithCharset(sb, lowerCaseLetters, length)
+}
+
+func UpperCaseLetters(sb RuneWriter, length int) {
+	stringWithCharset(sb, upperCaseLetters, length)
+}
+
+func AlphaNumeric(sb RuneWriter, length int) {
+	stringWithCharset(sb, alphaUpperNumeric, length)
+}
+
+func stringWithCharset(sb RuneWriter, charset string, length int) {
+	for i := 0; i < length; i++ {
+		_, _ = sb.WriteRune(rune(charset[rand.Intn(len(charset))]))
+	}
+}
